78b034877897307655aedfa3a866b1b78ce8f957
Sathvik <sathvikkulal111@gmail.com>
2025-05-21 20:38:13 +0530

    first commit

---
+++ static/todo.js
@@ -0,0 +1,21 @@
+let display = document.getElementById("display");
+
+// Append value to the display
+function appendValue(value) {
+    display.value += value;
+}
+
+// Clear the display
+function clearDisplay() {
+    display.value = "";
+}
+
+// Calculate the result
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
+
